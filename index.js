f7e8d27a064935cc3266ba9908fc4ec1609d490c
Md-Pervez-Hossain <p.hossain9254@gmail.comm>
2022-11-08 13:55:39 +0600

    find a single item

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.zr5yxev.mongodb.net/?retryWrites=true&w=majority`;
 console.log(uri);
 const client = new MongoClient(uri, {
@@ -25,6 +25,13 @@ async function run() {
       const query = {};
       const cursor = foodServiceCollection.find(query);
       const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.get("/addservice/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await foodServiceCollection.findOne(query);
       res.send(result);
     });
 
